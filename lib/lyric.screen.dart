cea5ce9eed01cd1efec9130eb3ed13330fb9b1a3
Han Li <vcfvct@gmail.com>
2019-12-10 08:46:13 -0500

    added video-search. fixed ios debug, but webview still blank in iOS emulator

--- lib/lyric.screen.dart
+++ lib/lyric.screen.dart
@@ -1,17 +1,46 @@
 import 'package:flutter/material.dart';
 import 'model/song.dart';
+import 'video.screen.dart';
 
 class LyricScreen extends StatelessWidget {
   final Song song;
   LyricScreen({Key key, @required this.song}) : super(key: key);
+  
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(
-          song.name,
-        ),
+        title: Text(song.name),
+        actions: <Widget>[
+          PopupMenuButton<String>(
+            onSelected: (choice) {
+              print(choice);
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => VideoScreen(keyword: song.name, site: choice),
+                ),
+              );
+            },
+            itemBuilder: (BuildContext ctx) {
+              return [
+                PopupMenuItem<String>(
+                  value: 'youtube',
+                  child: Text('Youtube'),
+                ),
+                PopupMenuItem<String>(
+                  value: 'qq',
+                  child: Text('腾讯视频'),
+                ),
+                PopupMenuItem<String>(
+                  value: 'iqiyi',
+                  child: Text('爱奇艺'),
+                ),
+              ];
+            },
+          )
+        ],
       ),
       body: Padding(
           padding: EdgeInsets.all(16.0),
---
+++ lib/video.screen.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class VideoScreen extends StatelessWidget {
+  final String keyword;
+  final String site;
+  VideoScreen({Key key, @required this.keyword, @required this.site})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('$keyword - 视频'),
+        ),
+        body: Stack(
+          children: <Widget>[
+            WebView(
+              initialUrl: buildSearchUrl(),
+              javascriptMode: JavascriptMode.unrestricted,
+              // onPageFinished: (finish) => _isLoadingPage = false,
+            ),
+          ],
+        ));
+  }
+
+  String buildSearchUrl() {
+    switch (site) {
+      case 'qq':
+        return 'https://v.qq.com/x/search/?q=$keyword';
+      case 'youtube':
+        return 'https://www.youtube.com/results?q=$keyword';
+      case 'iqiyi':
+        return 'https://so.iqiyi.com/so/q_$keyword';
+      default:
+        throw new Exception('Unknown site $site');
+    }
+  }
+}
